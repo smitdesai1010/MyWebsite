c863a4673ac082f098e33828d7fd6bca947377d0
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2020-06-05 00:50:43 -0400

    commit

--- website/src/Components/Contact/Contact.js
+++ website/src/Components/Contact/Contact.js
@@ -6,14 +6,12 @@ class Contact extends React.Component{
   render(){
     return (
        <div id='contact'>
-
          <h2 id='text'>Hey Let's Connect</h2>
 
          <div className='icons'>
           <Icon/>
           <Icon/>
          </div>
-
        </div>
     )
   }

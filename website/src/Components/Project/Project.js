4a887bb056612498916d0819bca02648b1196899
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2020-06-05 23:51:27 -0400

    commit

--- website/src/Components/Project/Project.js
+++ website/src/Components/Project/Project.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Project.css'
+import Card from './card.js'
 
 class Project extends React.Component{
   render(){
     return(
       <div id='project'>
         <div className='box'>
+          <Card/>
         </div>
       </div>
     )
---
+++ website/src/Components/Project/card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './card.css'
+
+class Card extends React.Component{
+  render(){
+    return(
+      <h1>Hey</h1>
+    )
+  }
+}
+
+export default Card;

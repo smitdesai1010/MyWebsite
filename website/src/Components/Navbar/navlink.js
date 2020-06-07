41c990b2828cd3cf71a3dbba0131ce15cc4754cc
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2020-06-07 00:27:37 -0400

    commit

--- website/src/Components/Navbar/navlink.js
+++ website/src/Components/Navbar/navlink.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 function Navlink(props){
   return (
-    <li><a href={props.href}>{props.name}</a></li>
+    <li>
+     <a href={props.href}> {props.name} </a>
+    </li>
   )
 }
 
--- website/src/Components/Project/card.js
+++ website/src/Components/Project/card.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import './card.css'
+import 'E:/Workspace/Personal-Website/website/node_modules/font-awesome/css/font-awesome.min.css';
 
 class Card extends React.Component{
   render(){
     return(
-      <h1>Hey</h1>
+        <div className='card'>
+         <img className='img' src={require('./encrpt.jpg')} alt='lock'/>
+         <div className='span'>
+           <div className='header'>
+             <div className='heading'>Encrypt.io</div>
+             <ul className='technologies'>
+               <li className='tech'>C++</li>
+             </ul>
+           </div>
+           <div className='plus'>
+            <i className="fa fa-plus-circle"></i>
+           </div>
+         </div>
+        </div>
     )
   }
 }
